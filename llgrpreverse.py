2256c401df9e9d7ac18b284adfa26225c4a47a75
Ashlesha Pawar <95329958+Ashpawarash1@users.noreply.github.com>
2025-01-21 23:38:43 +0530

    Update

---
+++ llgrpreverse.py
@@ -0,0 +1,79 @@
+"""Node is defined as
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+"""
+class Solution:
+    def reverseKGroup(self, head, k):
+        # Code here
+        curr=head
+        prev=None
+        tempTail=None
+        tempHead=head
+        n=0
+        while curr:
+            temp=curr.next
+            curr.next=prev
+            prev=curr
+            curr=temp
+            n+=1
+            if n%k==0 or curr==None:
+                if tempTail:
+                    tempTail.next=prev
+                else:
+                    head=prev
+                prev=None
+                tempTail=tempHead
+                tempHead=curr
+        return head
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+
+
+if __name__ == '__main__':
+    t = int(input())  # Number of test cases
+    while t > 0:
+        llist = LinkedList()
+
+        # Read list values and push them to the LinkedList
+        values = list(map(int, input().split()))
+        for i in reversed(values):
+            llist.push(i)
+
+        k = int(input())  # Size of the group for reversal
+        ob = Solution()
+        new_head = ob.reverseKGroup(llist.head, k)
+        llist.head = new_head
+        llist.printList()  # Print the modified linked list
+        t -= 1
+
+        print("~")
+
+# } Driver Code Ends
